028b97a7edf7cfa12b79f1720e9d91ad43ac3d2c
sungjoonim <junjlim8513@gmail.com>
2025-11-28 13:17:59 +0900

    feat: zsh, oh-my-zsh 설치하는 init.sh 작성

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# zsh 설치
+sudo apt update
+sudo apt install -y zsh
+chsh -s "$(which zsh)"
+exec zsh
+
+# oh my zsh 설치
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
